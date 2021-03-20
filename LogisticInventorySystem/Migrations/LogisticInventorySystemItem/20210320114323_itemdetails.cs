a7752c957ee7e7ac4a71863c3976d16152cbe0ea
JAGDEESH SINGH A L THARA SINGH <TP046012@mail.apu.edu.my>
2021-03-20 20:49:27 +0800

    published to azure

---
+++ LogisticInventorySystem/Migrations/LogisticInventorySystemItem/20210320114323_itemdetails.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LogisticInventorySystem.Migrations.LogisticInventorySystemItem
+{
+    public partial class itemdetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Item",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemID",
+                table: "Item",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "ItemID",
+                table: "Item");
+        }
+    }
+}
